c0674a1ccea01289c762f989bda8c30b2490f37f
Andrea Piras <37103053+thisispivi@users.noreply.github.com>
2025-01-26 07:05:57 +0100

    fix: eslint

--- tennisdle/src/assets/index.ts
+++ tennisdle/src/assets/index.ts
@@ -1,3 +1,6 @@
 import UnitedKingdomIcon from "./flags/UnitedKingdom.svg";
 import ItalyIcon from "./flags/Italy.svg";
 export { UnitedKingdomIcon, ItalyIcon };
+
+import ChevronIcon from "./icons/Chevron.svg";
+export { ChevronIcon };
--- tennisdle/src/index.ts
+++ tennisdle/src/index.ts
@@ -1,22 +1,3 @@
 import UnitedKingdomIcon from "./flags/UnitedKingdom.svg";
 import ItalyIcon from "./flags/Italy.svg";
 export { UnitedKingdomIcon, ItalyIcon };
-
-import ChevronIcon from "./icons/Chevron.svg";
-import FacebookIcon from "./icons/Facebook.svg";
-import GithubIcon from "./icons/Github.svg";
-import InstagramIcon from "./icons/Instagram.svg";
-import LinkedInIcon from "./icons/LinkedIn.svg";
-import SkypeIcon from "./icons/Skype.svg";
-import XIcon from "./icons/X.svg";
-import YoutubeIcon from "./icons/Youtube.svg";
-export {
-  ChevronIcon,
-  FacebookIcon,
-  GithubIcon,
-  InstagramIcon,
-  LinkedInIcon,
-  SkypeIcon,
-  XIcon,
-  YoutubeIcon,
-};
